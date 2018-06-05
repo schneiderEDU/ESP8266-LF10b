fabe2cad8c2ecd1f64701a8c7012a8c178e8210c
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2018-06-05 22:35:14 +0200

    Added counter

--- 01 - Hello World/01_hello_world.ino
+++ 01 - Hello World/01_hello_world.ino
@@ -1,8 +1,12 @@
+int i = 1;
+
 void setup () {
   Serial.begin(9600);
   Serial.println("Setup");
 }
 void loop () {
-  Serial.println("Loop");
+  Serial.print (i)
+  Serial.println(". Loop");
+  i++;
   delay(1000);
 }
