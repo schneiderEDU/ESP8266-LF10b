a0667a7d87dbe9a7cf5ebec51b9eb20784ce1b5a
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2018-06-05 22:54:10 +0200

    added comments

--- 01 - Hello World/01_hello_world.ino
+++ 01 - Hello World/01_hello_world.ino
@@ -1,12 +1,17 @@
 int i = 1;
 
+//setup() wird zum Start des SoCs genau einmal ausgeführt
 void setup () {
+  //Initialisiere serielle Schnittstelle auf 9600 Baud
   Serial.begin(9600);
   Serial.println("Setup");
 }
+
+//loop() wird bis zum Spannungsverlust des SoCs zyklisch durchlaufen
 void loop () {
   Serial.print (i)
   Serial.println(". Loop\n");
   i++;
+  //Warte 1000ms
   delay(1000);
 }
