a69443c56990a74675661aa1e027b07e055f3844
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2018-07-17 15:39:22 +0200

    Updated to match functions with uint values

--- 04 - RGB-LED-Shield/01 - Simple Effects/simple_effects.ino
+++ 04 - RGB-LED-Shield/01 - Simple Effects/simple_effects.ino
@@ -6,6 +6,7 @@
 
 Adafruit_NeoPixel led = Adafruit_NeoPixel(NUMPIX, PIN, NEO_GRB + NEO_KHZ800);
 
+//Helper function: turns all leds off
 void allOff() {
   for(int i = 0; i < NUMPIX; i++) {
     led.setPixelColor(i, 0);
@@ -13,13 +14,7 @@ void allOff() {
   led.show();
 }
 
-void setAll(int r, int g, int b) {
-  for(int i = 0; i < NUMPIX; i++) {
-    led.setPixelColor(i, r, g, b);
-  }
-  led.show();
-}
-
+//Helper function: set all leds to a specific color
 void setAll(uint32_t c) {
   for(int i = 0; i < NUMPIX; i++) {
     led.setPixelColor(i, c);
@@ -27,7 +22,7 @@ void setAll(uint32_t c) {
   led.show();
 }
 
-
+//fan-like rotation effect
 void rotation(uint32_t color, int time) {
     led.setPixelColor(0, color);
     for(int i=1; i<=(int)NUMPIX/2; i++) {
@@ -40,26 +35,30 @@ void rotation(uint32_t color, int time) {
     }
 }
 
-void pulse(byte r, byte g, byte b) {
-    led.setPixelColor(0, 0);
-    led.show();
-    float red, green, blue;
+//pulse effect
+void pulse(uint32_t color) {
+    //converting from unsigned integer to rgb values
+    byte red   = (byte)(color >> 16);
+    byte green = (byte)(color >>  8);
+    byte blue  = (byte)(color >>  0);
+    float r,g,b;
     for(int i = 0; i < 256; i++) {
-      red   = (i/256.0)*r;
-      green = (i/256.0)*g;
-      blue  = (i/256.0)*b;
-      setAll(red, green, blue);
+      r   = (i/256.0)*red;
+      g   = (i/256.0)*green;
+      b   = (i/256.0)*blue;
+      setAll(led.Color(r, g, b));
       delay(10);
     }
     for(int i = 256; i > 0; i--) {
-      red   = (i/256.0)*r;
-      green = (i/256.0)*g;
-      blue  = (i/256.0)*b;
-      setAll(red, green, blue);
+      r   = (i/256.0)*red;
+      g   = (i/256.0)*green;
+      b   = (i/256.0)*blue;
+      setAll(led.Color(r, g, b));
       delay(10);
     }
 }
 
+//strobe effect
 void strobe(uint32_t color, int flashCount, int flashDelay) {
   for(int i = 0; i < flashCount; i++) {
     setAll(color);
@@ -77,6 +76,6 @@ void setup() {
 void loop() {
     led.setBrightness(64);
     rotation(led.Color(255, 15, 223), 1000);
-    pulse(0xFF, 0x0F, 0xDF);
+    pulse(led.Color(255, 15, 223));
     strobe(led.Color(255, 15, 223), 50, 50);
 }
