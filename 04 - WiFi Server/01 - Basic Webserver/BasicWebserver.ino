423fa781cb29084b815f589793fe5c60285e7137
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2018-08-01 19:56:27 +0200

    Added comments

--- 04 - WiFi Server/01 - Basic Webserver/BasicWebserver.ino
+++ 04 - WiFi Server/01 - Basic Webserver/BasicWebserver.ino
@@ -1,22 +1,31 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+//Webserver-Library
 #include <ESP8266WebServer.h>
 
 const char* ssid = "MYWIFISSID";
 const char* password = "mysupersecretpassword";
 
+//Definition des Ports, Standardport ist 80
 const int serverPort = 1337;
+//Serverobjekt mit Port erzeugen
 ESP8266WebServer server(serverPort);
 
+//Funktion für Aufruf des Wurzelverzeichnisses
 void handleRootPath() {
+    //Sende HTTP-Antwortcode 200 (OK) und Text
     server.send(200, "text/plain", "This is the root path");
 }
 
+//Funktion für Aufruf des /alt-Pfads
 void handleAltPath() {
+    //Sende HTTP-Antwortcode 200 (OK) und Text
     server.send(200, "text/plain", "This is the alternative path");
 }
 
+//Funktion für Aufruf aller anderen ungültigen Pfade
 void handleNotFound() {
+    //Sende HTTP-Antwortcode 404 (Not found) und Text
     server.send(404, "text/plain", "The requested URL was not found on this server.");
 }
 
@@ -32,13 +41,18 @@ void setup() {
     Serial.println();
     Serial.print("Connected as ");
     Serial.println(WiFi.localIP());
+    //Requesthandling für Wurzelverzeichnis
     server.on("/", handleRootPath);
+    //Requesthandling für /alt - Pfad
     server.on("/alt", handleAltPath);
+    //Requesthandling für alle anderen Pfade
     server.onNotFound(handleNotFound);
+    //Starten des Serverdienstes
     server.begin();
     Serial.printf("[Server] Server is listening on Port %i", serverPort);
 }
 
 void loop() {
+    //Behandeln eingehender Verbindungen
     server.handleClient();
 }
