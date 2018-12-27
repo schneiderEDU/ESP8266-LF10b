07d8fce70f9c920ac9bb95aaf55a8d2211be427e
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2018-12-27 15:48:10 +0100

    Initial commit

---
+++ 02 - WiFi/06 - mDNS/README.md
@@ -0,0 +1,26 @@
+# 06 - mDNS (Multicast DNS)
+
+
+
+## Grundlagenwissen
+
+
+## Weiterentwicklung
+
+
+## Weiterführende Literatur
+
+* [Scan Class](https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/scan-class.html) - Dokumentation der Scan-Klasse der *ESP8266WiFi.h*-Bibliothek
+
+## Entwickelt mit
+
+* [Atom](https://atom.io/) - Ein frei konfigurierbarer Texteditor
+* [Platform.io](https://platformio.org/) - Eine Open-Source-Entwicklungsumgebung für die Entwicklung von IoT-Systemen
+
+## Autor
+
+* **Christian Schneider** - [schneiderEDU](https://github.com/schneiderEDU)
+
+## Lizenz
+
+Dieses Projekt wurde unter der MIT License veröffentlicht -  [LICENSE.md](LICENSE.md) für Details
---
+++ 02 - WiFi/06 - mDNS/mDNS.ino
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+
+
+//WiFi credentials - Festlegen der WLAN-Verbindungsdaten
+const char* ssid = "MYWIFISSID";
+const char* password = "mysupersecretpassword";
+//Name, auf den später per DNS aufgelöst werden soll
+const char* name = "myesp";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ");
+  Serial.print(ssid);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  //Ausgabe bei einer erfolgreichen Verbindung, inkl. vom DHCP-Server erhaltenen IP-Adresse
+  Serial.print("Connected as ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Starting mDNS responder...");
+  if(!MDNS.begin(name)) {
+    Serial.println("failed!");
+  }
+  else {
+    Serial.println("successful!");
+    Serial.print("Name: ");
+    Serial.print(name);
+    Serial.println(".local");
+  }
+}
+
+void loop() {
+
+}
